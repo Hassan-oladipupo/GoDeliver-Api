fe8b0ade39d6ce3eb5439287a9d6ef1d21928c51
Hassan Oladipupo <109126045+Hassan-oladipupo@users.noreply.github.com>
2024-06-24 08:29:32 +0100

    new commit

--- src/Controller/ProfileSettingController.php
+++ src/Controller/ProfileSettingController.php
@@ -14,7 +14,6 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
